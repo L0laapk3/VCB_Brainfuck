a52e209f34ea6dc9188e26ee3da744984b3aa2a6
L0laapk3 <L0laapk3@gmail.com>
2022-05-28 14:28:36 +0200

    added compiler

---
+++ compile.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def compile(bf):
+	references = []
+	chars = list(bf)
+	for i in range(len(chars)):
+		char = chars[i]
+		if char == '>':
+			chars[i] = "0xfe"
+		elif char == '<':
+			chars[i] = "0xfd"
+		elif char == '+':
+			chars[i] = "0xfb"
+		elif char == '-':
+			chars[i] = "0xf7"
+		elif char == '.':
+			chars[i] = "0xef"
+		elif char == ',':
+			chars[i] = "0xdf"
+		elif char == '[':
+			chars[i] = "ERROR"
+			references.append(i)
+		elif char == ']':
+			s = references.pop()
+			chars[s] = f"{hex(i + 1)}0bf"
+			chars[i] = f"{hex(s + 1)}07f"
+	return '\n'.join(chars)
+
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		bf = sys.argv[1]
+	else:
+		bf = input("Enter your brainfuck code: ")
+
+	asm = compile(bf)
+
+	print(asm)
+
